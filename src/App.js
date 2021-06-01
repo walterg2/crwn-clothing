9d0fceb6af5fc773705172bcb15a989e48b9d528
George Walters <walterg2@gmail.com>
2021-06-01 13:31:52 -0400

    Adding Collection and CollectionItem for the new Shop page

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
-import HatsPage from './pages/hats/hats.component';
+import ShopPage from './pages/shop/shop.compnent';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/shop/hats" component={HatsPage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
--- src/pages/hats/hats.component.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const HatsPage = () => (
-    <h1>Hats</h1>
-);
-
-export default HatsPage;
