8432fa61866d89d6ff5c84381dbcc19d1555543d
George Walters <walterg2@gmail.com>
2021-06-04 08:15:37 -0400

    Adding Redux Persist to the app
    We can now store our cart locally so it persists through
    refreshes/browser closures.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,15 @@
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
 
 const middleware = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middleware));
+export const store = createStore(rootReducer, applyMiddleware(...middleware));
 
-export default store;
+export const persistor = persistStore(store);
+
+const exportedMethods = { store, persistor };
+
+export default exportedMethods;
