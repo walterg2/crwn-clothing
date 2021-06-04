ff6a84973769129275fc6ceeef59ded4da65df11
George Walters <walterg2@gmail.com>
2021-06-04 10:01:27 -0400

    Moved data to reducers; Added explicit collection pages

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ class App extends React.Component {
         <Header />
         <Switch>
           <Route exact path="/" component={HomePage} />
-          <Route exact path="/shop" component={ShopPage} />
+          <Route path="/shop" component={ShopPage} />
           <Route exact path="/signin">
             { this.props.currentUser ?
                 <Redirect to='/' />
