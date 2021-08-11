e3151cb79bb57e7c279934c977ac43d8e8242576
George Walters <walterg2@gmail.com>
2021-08-11 13:28:50 -0400

    Adding Redux Thunk to the app and migrating Shop Collection fetching to it

--- src/pages/shop/shop.compnent.jsx
+++ src/pages/shop/shop.compnent.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import { createStructuredSelector } from 'reselect';
 import { connect } from 'react-redux';
 
 import CollectionPage from '../collection/collection.component';
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import WithSpinner from '../../components/with-spinner/with-spinner.component';
 
-import  { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
-
-import { updateCollections } from '../../redux/shop/shop.actions';
+import { fetchCollectionsStartAsync } from '../../redux/shop/shop.actions';
+import { selectIsCollectionFetching, selectIsCollectionsLoaded } from '../../redux/shop/shop.selector';
 
 import './shop.styles.scss';
 
@@ -16,39 +16,32 @@ const CollectionOverviewWithSpinner = WithSpinner(CollectionsOverview);
 const CollectionPageWithSpinner = WithSpinner(CollectionPage);
 
 class ShopPage extends React.Component {
-  state = {
-    loading: true
-  };
-
   unsubscribeFromSnapshot = null;
 
   componentDidMount() {
-    const { updateCollections } = this.props;
-
-    const collectionRef = firestore.collection('collections');
-    collectionRef.onSnapshot(async snapshot => {
-      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-      updateCollections(collectionsMap);
-
-      this.setState({ loading: false });
-    });
-  }
+    const { fetchCollectionsStartAsync } = this.props;
+    fetchCollectionsStartAsync();
+  };
 
   render() {
-    const { match } = this.props;
-    const { loading } = this.state;
+    const { match, isCollectionFetching, isCollectionsLoaded } = this.props;
 
     return (
       <div className="shop-page">
-        <Route exact path={`${match.path}`} render={(props => <CollectionOverviewWithSpinner isLoading={loading} {...props} />)} />
-        <Route path={`${match.path}/:collectionId`} render={(props => <CollectionPageWithSpinner isLoading={loading} {...props} />)} />
+        <Route exact path={`${match.path}`} render={(props => <CollectionOverviewWithSpinner isLoading={isCollectionFetching} {...props} />)} />
+        <Route path={`${match.path}/:collectionId`} render={(props => <CollectionPageWithSpinner isLoading={!isCollectionsLoaded} {...props} />)} />
       </div>
     );
   };
 };
 
-const mapDispatchToProps = dispatch => ({
-  updateCollections: collectionsMap => dispatch(updateCollections(collectionsMap))
-})
+const mapStateToProps = createStructuredSelector({
+  isCollectionFetching: selectIsCollectionFetching,
+  isCollectionsLoaded: selectIsCollectionsLoaded
+});
 
-export default connect(null, mapDispatchToProps)(ShopPage);
+const mapDispatchToProps = dispatch => ({
+  fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ShopPage);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,11 @@
 import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 
 import rootReducer from './root-reducer';
 
-const middleware = [logger];
+const middleware = [logger, thunk];
 
 export const store = createStore(rootReducer, applyMiddleware(...middleware));
 
