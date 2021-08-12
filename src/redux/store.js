96203e4c981ad071f5b116f8a5d52da1bb6cf02f
George Walters <walterg2@gmail.com>
2021-08-12 11:39:30 -0400

    Refactoring to takeLatest; Setting up ability to concurrently run sagas without adding lots of run statements

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,8 +4,7 @@ import logger from 'redux-logger';
 import createSagaMiddleware from 'redux-saga';
 
 import rootReducer from './root-reducer';
-
-import { fetchCollectionStart } from './shop/shop.sagas';
+import rootSaga from './root-saga';
 
 const sagaMiddleware = createSagaMiddleware();
 const middleware = [sagaMiddleware];
@@ -16,7 +15,7 @@ if (process.env.NODE_ENV === 'development') {
 
 export const store = createStore(rootReducer, applyMiddleware(...middleware));
 
-sagaMiddleware.run(fetchCollectionStart);
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
 
